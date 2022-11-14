45ed53dc3e8ebc2349ffd86d80155d381a953da6
kateolefir <kateryna.olefir@gmail.com>
2022-11-14 11:03:37 +0200

    fix transactions

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import styles from './styles.module.css';
+import styles from '../TransactionHistory/styles.module.css';
 
 function TransactionHistory({ items }) {
   return (
@@ -7,14 +7,14 @@ function TransactionHistory({ items }) {
       <table className={styles.transactionHistory}>
         <thead>
           <tr>
-            <th className={styles.head}>Type</th>
-            <th className={styles.head}>Amount</th>
-            <th className={styles.head}>Currency</th>
+            <th className={styles.tsbleHead}>Type</th>
+            <th className={styles.tsbleHead}>Amount</th>
+            <th className={styles.tsbleHead}>Currency</th>
           </tr>
         </thead>
 
         <tbody>
-          {items.map(({ id, type, amount, currency }) => (
+          {items?.map(({ id, type, amount, currency }) => (
             <tr key={id} className={styles.row}>
               <td className={styles.col}>{type}</td>
               <td className={styles.col}>{amount}</td>
